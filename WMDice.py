31deeeb56510182e6d1065c092e076ca6b86b94a
Evan Hurwitz <evan.hurwitz@avast.com>
2026-05-08 14:46:57 +0100

    added summing logic

--- WMDice.py
+++ WMDice.py
@@ -9,10 +9,21 @@ def RollSet (n_dice: int, n_rolls: int):
     # sums = rolls.sum(axis=1)
     return rolls
 
+def SumRolls (rolls: np.ndarray, offset: int):
+    sums = rolls.sum(axis=1) + offset
+    return sums
 
 
 if __name__ == '__main__':
     n_dice = 3
     n_rolls = 8
     rolls = RollSet(n_dice, n_rolls)
+    offset = 1 #Dice + 1
+    sums = SumRolls(rolls, offset)
+    print(f"Rolling {n_dice} dice {n_rolls} times:")
+    print(rolls)
+    print(f"Sums of rolls with offset {offset}:")
+    print(sums)
+
+
 
